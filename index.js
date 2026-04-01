7714f23632069b493c64684352e5d6a114aa3e8b
Garv Kumar <itsgarvv@gmail.com>
2026-04-01 23:09:03 +0530

    updated Responsiveness and Scroll Behaviour.

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-let hamburger_menu = document.getElementById("hamburger_menu");
-    let cross_menu = document.getElementById("cross_menu");
-    let hidden_menu = document.getElementById("hidden_menu");
-    
-    function showMenu() {
-        hamburger_menu.style.display = "none";
-        cross_menu.style.display = "flex";
-        hidden_menu.style.display = "flex";
-    }
+const hamburger_menu = document.getElementById("hamburger_menu");
+const cross_menu = document.getElementById("cross_menu");
+const hidden_menu = document.getElementById("hidden_menu");
+const navbar = document.querySelector(".navbar");
 
-    function hideMenu() {
-        hamburger_menu.style.display = "flex";
-        cross_menu.style.display = "none";
-        hidden_menu.style.display = "none";
-    }
+hamburger_menu.addEventListener("click", () => {
+    hamburger_menu.style.display = "none";
+    cross_menu.style.display = "flex";
+    hidden_menu.style.display = "flex";
+    navbar.style.borderBottom = "2px solid #fff";
+})
+
+cross_menu.addEventListener("click", () => {
+    hamburger_menu.style.display = "flex";
+    cross_menu.style.display = "none";
+    hidden_menu.style = "none";
+})
+
+const animate = document.querySelectorAll(".animate");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("scroll-animate");
+        } else {
+            entry.target.classList.remove("scroll-animate");
+        }
+    })
+}, { threshold: 0.5 });
+
+for (let i = 0; i < animate.length; i++) {
+    const element = animate[i];
+    observer.observe(element);
+}
+
